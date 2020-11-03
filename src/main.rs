63c50aa1b5c41b540f8dbdec21f96716129e4507
jordi kaucasus <jordi@kaucasus.com>
2020-11-03 02:11:30 +0100

    Added basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate image;
+
+use image::GenericImageView;
+
 fn main() {
     println!("Hello, world!");
+    let img = image::open("tests/base_test_image.png").unwrap();
+
+    println!(
+        "Image dimensions are: {:?}, and mode is {:?}",
+        img.dimensions(),
+        img.color()
+    );
+
+    let mut memory_init_vector = String::new();
+
+    for pixel in img.pixels() {
+        memory_init_vector += &pixel_to_str(pixel.2);
+        //There's probably an easier way to do this. We have an iterator after all...
+        if pixel.0 == img.width() - 1 {
+            if pixel.1 == img.height() - 1 {
+                memory_init_vector += ";\n";
+            } else {
+                memory_init_vector += ",\n";
+            }
+        } else {
+            memory_init_vector += " ";
+        }
+    }
+
+    println!("{:};", memory_init_vector);
+}
+
+#[derive(Debug)]
+struct Mode {
+    memwidth: i32,
+    area: i32,
+    alpha: u8,
+}
+
+fn pixel_to_str(pixel: image::Rgba<u8>) -> String {
+    format!("{:08b}{:08b}{:08b}", pixel[0], pixel[1], pixel[2])
 }
